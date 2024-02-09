dcfaafec9d5fe9564f3994e93061e58819f2180a
Adrien Osakwe <adrien.osakwe@mail.mcgill.ca>
2024-02-09 14:35:35 -0500

    Added example plot

--- example.py
+++ example.py
@@ -7,3 +7,13 @@ print('hello world')
 count = int(input("Enter a number: "))
 
 print(count)
+
+
+x = np.arange(count)
+
+y = 2*x + np.random.randn(len(x))
+
+
+plt.plot(x,y)
+
+plt.savefig('example_nontext.png')
